78676dd34c7b9e1aec5ba99dcbc37abe60d51119
Foxbane <you@example.com>
2021-12-12 22:58:09 +0000

    main

--- main.js
+++ main.js
@@ -1,42 +1,44 @@
+//Declaration of constants and imports of packages needed fro app
 const { app, BrowserWindow, Menu, ipcMain, shell, ipcRenderer } = require('electron')
-const {ChildProcess, exec, spawn} = require('child_process')
+const {ChildProcess, exec, spawn, execFile} = require('child_process')
 const log = require('electron-log')
 const { Console, error } = require('console')
-const { stdout } = require('process')
+const { stdout, stderr } = require('process')
+const fs= require('fs')
+//declariation of global variables, not ideal but should be ok for localised applcation.
 var DID
 var target
-globalThis.file
-globalThis.file_location
+var file
+var file_location
 
 // Set env
 process.env.NODE_ENV = 'development'
 
 const isDev = process.env.NODE_ENV !== 'production' ? true : false
-const isMac = process.platform === 'darwin' ? true : false
 
 let mainWindow
 
+//settings for app window 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
     title: 'Venator',
-    width: isDev ? 1200 : 800,
-    height: 900,
-    wdith: 600,
-    icon: './assets/icons/icon.png',
+    width: isDev ? 900 : 600,
+    height: 600,
+    width: 450,
     resizable: isDev ? true : false,
-    backgroundColor: 'darkslateblue',
+    backgroundColor: '#483d8b',
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
-    },
-  })
+    }
+  });
 
   if (isDev) {
     mainWindow.webContents.openDevTools()
   }
   mainWindow.loadFile('./app/index.html')
 }
-
+//launcher 
 app.on('ready', () => {
   createMainWindow()
   const mainMenu = Menu.buildFromTemplate(menu)
@@ -44,7 +46,6 @@ app.on('ready', () => {
 })
 
 const menu = [
-  ...(isMac ? [{ role: 'appMenu' }] : []),
   {
     role: 'fileMenu',
 
@@ -64,35 +65,74 @@ const menu = [
     : []),
 ]
 
+//closing window, stop and remove active contianer
 app.on('window-all-closed', () => {
-  if (!isMac) {
     exec('docker kill'+" "+DID)
     exec('docker rm'+" "+DID)
     app.quit()
-  }
 })
-
+//Launching window, check docker is on the system
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createMainWindow()
+    var commandExists = require('command-exists');  
+    commandExists('docker').then( {
+    }).catch(mainWindow.webContents.send('Docker:error'), {
+    });
   }
 })
 
+//Communicaiton with frontend 
 ipcMain.on('send:cmd', (event,args) =>{
   console.log(args);
-  sendCommand(args);
+  startContainer(args);
 })
+//test selector with switch statement
 ipcMain.on('start:analysis', (event,args) =>{
-  console.log(args);
-  startAnalysis(args);
+  console.log(args)
+  test=args.test_type
+  console.log(test)
+  switch(test){
+    case 'exiftool':
+      runExif(args);
+      break;
+    case 'zipdump.py':
+      runZipdump(args);
+      break;
+    case 'diec':
+      runDiec(args);
+      break;
+    default:
+      mainWindow.webContents.send('Error:encountered',`${stderr}`);
+      break;
+  }
 })
+//Takes file location and splits down for opening with notepad
+ipcMain.on('open:file', (event,args) =>{
+    let results= args.results_location;
+    results1=results.replaceAll('"', '')
+    console.log(results1)
+    results2=results1.slice(1)
+    console.log(results2)
+    openFile(results2);
+    results2=""
+});
+ipcMain.on('reload', (event)=> {
+  exec('docker kill'+" "+DID)
+  exec('docker rm'+" "+DID)
+  win.loadFile('index.html')
+});
+ipcMain.on('retest', (event)=>{
+  mainWindow.webContents.send('setup:end',DID);
+});
 
-async function sendCommand(args){
+//Process Functions
+//function for spinning up container
+async function startContainer(args){
   target = args.target
   file = args.filen
   file_location = (":home/remnux/"+file)
   console.log(target,file);
-
   const dockers = exec('docker run --detach remnux/remnux-distro');
     dockers.stdout.on('data', (data) => {          
       let dockerID=(`${data}`)  
@@ -102,50 +142,128 @@ async function sendCommand(args){
     });
     dockers.stderr.on('data', (data) => {
       console.error(`stderr: ${data}`);
+      mainWindow.webContents.send('Error:encountered',`${data}`);
     });
     dockers.on('close', (code) => {
-     console.log(`child process exited with code ${code}`);
-     sendFile();
+     if(code==0){
+      console.log(`child process exited with code ${code}`);
+      sendFile();
+     }
     });
 }
-  async function sendFile(){
-    console.log("the container id is "+ DID);
-    let filep= (DID+":home/remnux/")
-    console.log(filep)
-    console.log('docker cp'+" "+target+" "+filep)
-    const transfer = exec('docker cp'+" "+target+" "+filep)
-      transfer.stdout.on('data', (data) => {    
-        console.log(`${data}`)          
-      });
-      
-      transfer.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-      });
-      
-      transfer.on('close', (code) => {
-      console.log(`child process exited with code ${code}`);
-      });
-      console.log(`${stdout}`)
-      mainWindow.webContents.send('setup:end',DID);
-  }
+//copies file from host to container
+async function sendFile(){
+  console.log("the container id is "+ DID);
+  let filep= (DID+":home/remnux/")
+  console.log(filep)
+  console.log('docker cp'+" "+target+" "+filep)
+  const transfer = exec('docker cp'+" "+target+" "+filep)
+    transfer.stdout.on('data', (data) => {    
+      console.log(`${data}`)          
+    });
 
-async function startAnalysis(args){
+    transfer.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+      mainWindow.webContents.send('Error:encountered',`${data}`);
+    });
+  
+    transfer.on('close', (code) => {
+      if(code==0){
+        console.log(`child process exited with code ${code}`);
+        console.log(`${stdout}`)
+        mainWindow.webContents.send('setup:end',DID);
+      }
+    });
+}
+//opens the output file with notepad
+async function openFile(){
+  console.log(results2)
+  const notepad= exec('notepad'+" "+results2)
+    notepad.stdout.on('data', (data) => {    
+      console.log(`${data}`)
+    });
+
+    notepad.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+      mainWindow.webContents.send('Error:encountered',`${data}`);
+    });
+
+    notepad.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+        if (code==0) {
+        }
+    });
+}
+
+//Functions for Tests 
+async function runExif(args){
   let test_type = args.test_type
-  let output_location= ("> app/output/"+file+"_output.txt")
+  let output_location= (">app/output/"+file+"_exif.txt")
+  console.log(test_type);
+  console.log(output_location);
+
+  const starttest= exec('docker exec'+" "+DID+ " "+ test_type+ " "+file+" "+output_location)
+    starttest.stdout.on('data', (data) => {    
+      console.log(`${data}`)
+    });
+
+    starttest.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+      mainWindow.webContents.send('Error:encountered',`${data}`);
+    });
+
+    starttest.on('close', (code) => {
+     console.log(`child process exited with code ${code}`);
+     if (code==0) {
+      console.log(`${stdout}`)
+      mainWindow.webContents.send('Analysis:complete',output_location);
+     }
+    });
+}
+async function runZipdump(args){
+  let test_type = args.test_type
+  let output_location= (">app/output/"+file+"_zdump.txt")
   console.log(test_type);
   console.log(output_location);
   const starttest= exec('docker exec'+" "+DID+ " "+ test_type+ " "+file+" "+output_location)
     starttest.stdout.on('data', (data) => {    
       console.log(`${data}`)
-
     });
+
     starttest.stderr.on('data', (data) => {
       console.error(`stderr: ${data}`);
+      mainWindow.webContents.send('Error:encountered',`${data}`);
     });
+
     starttest.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-    
+     console.log(`child process exited with code ${code}`);
+     if (code==0) {
+      console.log(`${stdout}`)
+      mainWindow.webContents.send('Analysis:complete',output_location);
+     }
     });
-    console.log(`${stdout}`)
-    mainWindow.webContents.send('Analysis:complete',output_location);
+}
+async function runDiec(args){
+  let test_type = args.test_type
+  let output_location= (">app/output/"+file+"_sysdig.txt")
+  console.log(test_type);
+  console.log(output_location);
+
+  const starttest= exec('docker exec'+" "+DID+ " "+ test_type+ " "+"-d"+" "+file+" "+output_location)
+    starttest.stdout.on('data', (data) => {    
+      console.log(`${data}`)
+    });
+
+    starttest.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+      mainWindow.webContents.send('Error:encountered',`${data}`);
+    });
+
+    starttest.on('close', (code) => {
+     console.log(`child process exited with code ${code}`);
+     if (code==0) {
+      console.log(`${stdout}`)
+      mainWindow.webContents.send('Analysis:complete',output_location);
+     }
+    });
 }
